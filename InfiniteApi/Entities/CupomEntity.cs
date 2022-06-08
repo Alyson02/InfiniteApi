c7da380c50d7312020a526bd233d5b7e28db4c29
Alyson <80166983+Alyson02@users.noreply.github.com>
2022-06-07 22:40:51 -0300

    Adicionar arquivos de projeto.

---
+++ InfiniteApi/Entities/CupomEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InfiniteApi.Entities
+{
+    public class CupomEntity
+    {
+        [Key()]
+        public int CupomId { get; set; }
+        public char Tipo { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
