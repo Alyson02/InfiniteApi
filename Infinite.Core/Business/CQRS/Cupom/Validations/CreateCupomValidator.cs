1a9add27a783af95ae847163c2592aaedbc5debf
Samuel-sys <Alencar5647@gmail.com>
2022-06-24 22:11:21 -0300

    Validetor de Cupom

--- Infinite.Core/Business/CQRS/Cupom/Validations/CreateCupomValidator.cs
+++ Infinite.Core/Business/CQRS/Cupom/Validations/CreateCupomValidator.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
 using Infinite.Core.Business.CQRS.Cupom.Commands;
+using Infinite.Core.Business.Services.Base;
+using Infinite.Core.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,23 @@ using System.Threading.Tasks;
 
 namespace Infinite.Core.Business.CQRS.Cupom.Validations
 {
-    public class CreateCupomValidator : AbstractValidator<CreateCupomCommand>
+    internal class CreateCupomValidator : AbstractValidator<CreateCupomCommand>
     {
-        public CreateCupomValidator()
+        public CreateCupomValidator(IServiceBase<CupomEntity> service)
         {
+            RuleFor(x => x.TipoCupomId)
+                .Custom((TipoCupom, contexto) =>
+                {
+                    var spec = service.CreateSpec(x => x.TipoCupomId == TipoCupom)
+                        .AddInclude(x => x.TipoCupom);
+                    var temTipoCupom = service.FindAsync(spec).Result;
+
+                    if (temTipoCupom == null)
+                    {
+                        contexto.AddFailure("ID de Tipo de cupom não cadastrado");
+                    }
+                });
         }
+
     }
 }
