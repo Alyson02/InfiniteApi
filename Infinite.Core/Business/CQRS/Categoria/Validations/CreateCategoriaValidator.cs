a07ca89bc2c5f153667b557853ff236507c0d4c5
Samuel-sys <Alencar5647@gmail.com>
2022-06-24 21:57:22 -0300

    Validator de Categoria

---
+++ Infinite.Core/Business/CQRS/Categoria/Validations/CreateCategoriaValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Infinite.Core.Business.CQRS.Categoria.Commands;
+using Infinite.Core.Business.Services.Base;
+using Infinite.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infinite.Core.Business.CQRS.Categoria.Validations
+{
+    internal class CreateCategoriaValidator : AbstractValidator<CreateCategoriaCommand>
+    {
+        public CreateCategoriaValidator(IServiceBase<CategoriaEntity> service)
+        {
+            RuleFor(x => x.Categoria)
+                .MaximumLength(50)
+                .NotEmpty();
+        }
+
+    }
+}
